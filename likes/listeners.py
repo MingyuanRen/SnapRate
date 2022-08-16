b41fce932dc3a329c67a3879343bb8631f6fc07f
Mingyuan Ren <m28ren@uwaterloo.ca>
2022-08-16 19:07:09 -0400

    likes listeners

--- likes/listeners.py
+++ likes/listeners.py
@@ -10,11 +10,7 @@ def incr_likes_count(sender, instance, created, **kwargs):
 
     model_class = instance.content_type.model_class()
     if model_class != Tweet:
-        # TODO HOMEWORK 给 Comment 使用类似的方法进行 likes_count 的统计
         return
-
-    # 不可以使用 tweet.likes_count += 1; tweet.save() 的方式
-    # 因此这个操作不是原子操作，必须使用 update 语句才是原子操作
     Tweet.objects.filter(id=instance.object_id).update(likes_count=F('likes_count') + 1)
     tweet = instance.content_object
     RedisHelper.incr_count(tweet, 'likes_count')
@@ -26,7 +22,6 @@ def decr_likes_count(sender, instance, **kwargs):
 
     model_class = instance.content_type.model_class()
     if model_class != Tweet:
-        # TODO HOMEWORK 给 Comment 使用类似的方法进行 likes_count 的统计
         return
 
     # handle tweet likes cancel
